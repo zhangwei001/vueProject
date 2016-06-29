30673774d92dbab6a944febf50cf413ffcffccae
zhangwei001 <zhangwei6765@163.com>
2016-06-29 15:35:06 +0800

    get string array repeat words

--- resources/js/component/public_methd.js
+++ resources/js/component/public_methd.js
@@ -1,1 +1,15 @@
 //常用数组方法
+/**
+ * 统计 字符串 数组重重复的字符串个数
+ */
+
+var arr =["apple","pear","orgian","apple","desk","pear","hello","origian"];
+
+var getWordsCnt =function(stringArray){
+         var retObj={};
+    stringArray.forEach(function(item,index){
+        retObj[item] = (retObj[item] +1) || 1;
+    })
+
+    return retObj;
+}
